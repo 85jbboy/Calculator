471fafbc71a5f3d25ef9ef06ab9e0a8c313f103c
85jbb <85jbb@MazERunnER>
2017-12-02 19:10:53 +0900

    basic function complete

--- src/arithmaticOperation/ArithmeticOperation.java
+++ src/arithmaticOperation/ArithmeticOperation.java
@@ -3,73 +3,40 @@ package arithmaticOperation;
 import java.util.Scanner;
 
 public class ArithmeticOperation {
-
-	public int[] getNum() {
-		System.out.print("계산할 수 2개를 입력하세요: ");
-		Scanner sc = new Scanner(System.in);
-		String sInput = sc.nextLine();
-		String[] str_num = sInput.split(" ");
-		int[] num = new int[str_num.length];
-		
-		for (int i = 0; i < str_num.length; i++) {
-			num[i] = Integer.parseInt(str_num[i]);
-		}
-		return num;
-	}
-	
-	public void calPlus(int[] calNum) {
-		int sum = 0;
-		for(int i = 0; i < calNum.length; i++) {
-			sum += calNum[i];
-		}
-		System.out.println("덧셈: " + sum);
-	}
-	public void calMinus(int[] calNum) {
-		int minus = calNum[0];
-		for(int i = 1; i < calNum.length; i++) {
-			minus -= calNum[i];
-		}
-		System.out.println("뺄셈: " + minus);
-	}
-	public void calTimes(int[] calNum) {
-		int times = calNum[0];
-		for(int i = 1; i < calNum.length; i++) {
-			times *= calNum[i];
-		}
-		System.out.println("나눗셈: " + times);		
-	}
-	public void calDiv(int[] calNum) {
-		int div = calNum[0];
-		for(int i = 1; i < calNum.length; i++) {
-			div /= calNum[i];
-		}
-		System.out.println("나눗셈: " + div);
-	}
-	
-	public void showResult() {
-		
-	}
-	
+	static int tmpResult = 0;
 	public static void main(String[] args) {
-		ArithmeticOperation arith = new ArithmeticOperation();
-		int[] calNum = arith.getNum();
-		String oper = arith.getOperator();
+		Scanner scanner = new Scanner(System.in);
 		
-		if (oper.equals("+")) {
-			arith.calPlus(calNum);
-		} else if (oper.equals("-")) {
-			arith.calMinus(calNum);	
-		} else if (oper.equals("*")) {
-			arith.calTimes(calNum);
-		} else {
-			arith.calDiv(calNum);	
-		}		
-	}
+		System.out.print("첫번째 값 입력: ");
+		tmpResult = scanner.nextInt();
 
-	private String getOperator() {
-		System.out.print("언션자를 입력하세요: " );
-		Scanner sc = new Scanner(System.in);
-		String operator = sc.next();
-		return operator;
+		
+		while (true) {
+			System.out.print("사칙연산 기호: ");
+			String operator = scanner.next();
+
+			if (operator.equals("quit")) {
+				System.out.println("최종 결과값: " + tmpResult);
+				return;
+			}
+			
+			System.out.print("두 번째 값 입력: ");
+			int nextNum= scanner.nextInt();
+
+			if (operator.equals("+")) {
+				tmpResult += nextNum;
+				System.out.println("현재값: " + tmpResult);
+			} else if (operator.equals("-")) {
+				tmpResult -= nextNum;
+				System.out.println("현재값: " + tmpResult);
+			} else if (operator.equals("*")) {
+				tmpResult *= nextNum;
+				System.out.println("현재값: " + tmpResult);
+			} else if (operator.equals("/")) {
+				tmpResult /= nextNum;
+				System.out.println("현재값: " + tmpResult);
+			} else 
+				System.out.println("사칙연산 기호가 아닙니다.");
+		}
 	}
 }
