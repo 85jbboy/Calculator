2a93f31984907b47e2f5152ea94eb2d0f29e7671
85jbb <85jbb@MazERunnER>
2017-12-02 16:24:25 +0900

    input operator

--- src/arithmaticOperation/ArithmeticOperation.java
+++ src/arithmaticOperation/ArithmeticOperation.java
@@ -22,28 +22,28 @@ public class ArithmeticOperation {
 		for(int i = 0; i < calNum.length; i++) {
 			sum += calNum[i];
 		}
-		System.out.println(sum);
+		System.out.println("덧셈: " + sum);
 	}
 	public void calMinus(int[] calNum) {
 		int minus = calNum[0];
 		for(int i = 1; i < calNum.length; i++) {
 			minus -= calNum[i];
 		}
-		System.out.println(minus);
+		System.out.println("뺄셈: " + minus);
 	}
 	public void calTimes(int[] calNum) {
 		int times = calNum[0];
 		for(int i = 1; i < calNum.length; i++) {
 			times *= calNum[i];
 		}
-		System.out.println(times);		
+		System.out.println("나눗셈: " + times);		
 	}
 	public void calDiv(int[] calNum) {
 		int div = calNum[0];
 		for(int i = 1; i < calNum.length; i++) {
 			div /= calNum[i];
 		}
-		System.out.println(div);
+		System.out.println("나눗셈: " + div);
 	}
 	
 	public void showResult() {
@@ -53,10 +53,23 @@ public class ArithmeticOperation {
 	public static void main(String[] args) {
 		ArithmeticOperation arith = new ArithmeticOperation();
 		int[] calNum = arith.getNum();
+		String oper = arith.getOperator();
 		
-		arith.calPlus(calNum);
-		arith.calMinus(calNum);
-		arith.calTimes(calNum);
-		arith.calDiv(calNum);
+		if (oper.equals("+")) {
+			arith.calPlus(calNum);
+		} else if (oper.equals("-")) {
+			arith.calMinus(calNum);	
+		} else if (oper.equals("*")) {
+			arith.calTimes(calNum);
+		} else {
+			arith.calDiv(calNum);	
+		}		
+	}
+
+	private String getOperator() {
+		System.out.print("언션자를 입력하세요: " );
+		Scanner sc = new Scanner(System.in);
+		String operator = sc.next();
+		return operator;
 	}
 }
